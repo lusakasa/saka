e96f598438987fc83219a490764234ab391e3e72
Uzair Shamim <uzashamim@gmail.com>
2018-04-30 09:14:36 -0400

    Added '+' between each key on hotkey list

--- src/options/Main/SakaHotkeysList/HotkeyListRow.jsx
+++ src/options/Main/SakaHotkeysList/HotkeyListRow.jsx
@@ -3,7 +3,12 @@ import { h } from 'preact';
 import 'material-components-web/dist/material-components-web.css';
 
 const HotkeyListRow = function HotkeyListRow({ title, keys }) {
-  const hotkeyShortcut = keys.map(key => <kbd>{key}</kbd>);
+  const hotkeyShortcut = keys.map((key, index, keysArray) => (
+    <span>
+      <kbd>{key}</kbd>
+      {keysArray.length === index + 1 ? '' : '+'}
+    </span>
+  ));
   return (
     <li className="mdc-list-item option">
       <span className="mdc-list-item__text">{title}</span>
