d643dfb2560dddc8465d262bb99cc672a020a537
Uzair Shamim <uzashamim@gmail.com>
2018-05-06 11:43:50 -0400

    Added alternate ctrl char for mac Added tooltip for customizing hotkeys

--- src/options/Main/SakaHotkeysList/index.jsx
+++ src/options/Main/SakaHotkeysList/index.jsx
@@ -1,6 +1,7 @@
 import { h } from 'preact';
 import HotkeyListRow from './HotkeyListRow.jsx';
 import 'material-components-web/dist/material-components-web.css';
+import { ctrlChar } from 'lib/utils';
 
 const SakaHotkeysList = function SakaHotkeysList({
   handleOpenSakaKeybindings
@@ -17,58 +18,83 @@ const SakaHotkeysList = function SakaHotkeysList({
         >
           arrow_back
         </i>
-        <h3 className="mdc-list-group__subheader">Keyboard Shortcuts</h3>
+        <div className="tooltip">
+          <i
+            id="custom-hotkey-info"
+            className="mdc-icon-toggle material-icons"
+            aria-pressed="false"
+            aria-label="Info about Saka custom hotkeys"
+          >
+            info
+          </i>
+          {SAKA_PLATFORM === 'chrome' ? (
+            <span className="tooltiptext">
+              To modify the Saka hotkeys, please visit
+              chrome://extensions/shortcuts
+            </span>
+          ) : (
+            <span className="tooltiptext">
+              It is currently not possible to modify hotkeys in firefox
+            </span>
+          )}
+        </div>
       </div>
+      <h3 className="mdc-list-group__subheader">Keyboard Shortcuts</h3>
       <div className="mdc-list-group">
         <ul className="mdc-list mdc-list--non-interactive mdc-list--dense">
-          <HotkeyListRow title="Open Saka" keys={['Ctrl', 'Space']} />
+          <HotkeyListRow title="Open Saka" keys={[ctrlChar, 'space']} />
           <li role="separator" className="mdc-list-divider options-separator" />
-          <HotkeyListRow title="Close Saka" keys={['Esc']} />
+          <HotkeyListRow title="Close Saka" keys={['esc']} />
           <li role="separator" className="mdc-list-divider options-separator" />
-          <HotkeyListRow title="Next Result" keys={['Tab']} />
+          <HotkeyListRow
+            title="Close Saka (when search bar is empty and focused)"
+            keys={['← backspace']}
+          />
           <li role="separator" className="mdc-list-divider options-separator" />
-          <HotkeyListRow title="Previous Result" keys={['Shift', 'Tab']} />
+          <HotkeyListRow title="Next Result" keys={['tab']} />
           <li role="separator" className="mdc-list-divider options-separator" />
-          <HotkeyListRow title="Clear Search" keys={['Ctrl', 'k']} />
+          <HotkeyListRow title="Previous Result" keys={['shift', 'tab']} />
+          <li role="separator" className="mdc-list-divider options-separator" />
+          <HotkeyListRow title="Clear Search" keys={[ctrlChar, 'k']} />
           <li role="separator" className="mdc-list-divider options-separator" />
           <HotkeyListRow
             title="Switch to next page of results"
-            keys={['Ctrl', 'd']}
+            keys={[ctrlChar, 'd']}
           />
           <li role="separator" className="mdc-list-divider options-separator" />
           <HotkeyListRow
             title="Switch To previous page of results"
-            keys={['Ctrl', 's']}
+            keys={[ctrlChar, 's']}
           />
           <li role="separator" className="mdc-list-divider options-separator" />
           <HotkeyListRow
             title="Switch Modes (when search bar is empty)"
-            keys={['Space']}
+            keys={['space']}
           />
           <li role="separator" className="mdc-list-divider options-separator" />
           <HotkeyListRow
             title="Switch Modes (when search bar not empty) "
-            keys={['Shift', 'Space']}
+            keys={['shift', 'space']}
           />
           <li role="separator" className="mdc-list-divider options-separator" />
           <HotkeyListRow
             title="Switch To Tabs Search"
-            keys={['Ctrl', 'Shift', 'a']}
+            keys={[ctrlChar, 'shift', 'a']}
           />
           <li role="separator" className="mdc-list-divider options-separator" />
           <HotkeyListRow
             title="Switch To Recently Closed Tabs Search"
-            keys={['Ctrl', 'Shift', 'c']}
+            keys={[ctrlChar, 'shift', 'c']}
           />
           <li role="separator" className="mdc-list-divider options-separator" />
           <HotkeyListRow
             title="Switch To Bookmark Search"
-            keys={['Ctrl', 'b']}
+            keys={[ctrlChar, 'b']}
           />
           <li role="separator" className="mdc-list-divider options-separator" />
           <HotkeyListRow
             title="Switch To History Search"
-            keys={['Ctrl', 'Shift', 'e']}
+            keys={[ctrlChar, 'shift', 'e']}
           />
         </ul>
       </div>
