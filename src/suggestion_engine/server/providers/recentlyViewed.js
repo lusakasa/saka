421f3ffd98526ccd92fff67d3a0f3e3e876f769d
Uzair Shamim <uzashamim@gmail.com>
2018-06-24 11:48:16 -0400

    Added workaround for chrome recentlyViewed

--- src/suggestion_engine/server/providers/recentlyViewed.js
+++ src/suggestion_engine/server/providers/recentlyViewed.js
@@ -3,7 +3,38 @@ import tabSuggestions, { allTabSuggestions } from './tab.js';
 import { getAllSuggestions as getAllClosedTabs } from './closedTab.js';
 import { allHistorySuggestions as getAllHistoryTabs } from './history.js';
 
-async function allRecentlyViewedSuggestions(searchString) {
+function compareRecentlyViewedSuggestions(suggestion1, suggestion2) {
+  return suggestion2.lastAccessed - suggestion1.lastAccessed;
+}
+
+async function getChromeRecentlyViewed(searchString) {
+  const openTabs = await tabSuggestions(searchString);
+  const closedTabs = await getAllClosedTabs(searchString);
+  const historyTabs = await getAllHistoryTabs(searchString);
+
+  const filteredClosedTabs = closedTabs.filter(tab =>
+    openTabs.every(openTab => openTab.url !== tab.url)
+  );
+
+  const filteredHistoryTabs = historyTabs.filter(tab =>
+    [...openTabs, ...filteredClosedTabs].every(
+      openOrClosedTab => openOrClosedTab.url !== tab.url
+    )
+  );
+
+  const sortedClosedAndHistoryTabs = [
+    ...filteredClosedTabs,
+    ...filteredHistoryTabs
+  ].sort(compareRecentlyViewedSuggestions);
+
+  return [...openTabs, ...sortedClosedAndHistoryTabs].map(tab => ({
+    ...tab,
+    originalType: tab.type,
+    type: 'recentlyViewed'
+  }));
+}
+
+async function getFirefoxRecentlyViewed(searchString) {
   const openTabs = await tabSuggestions(searchString);
   const closedTabs = await getAllClosedTabs(searchString);
   const historyTabs = await getAllHistoryTabs(searchString);
@@ -23,8 +54,12 @@ async function allRecentlyViewedSuggestions(searchString) {
   );
 }
 
-function compareRecentlyViewedSuggestions(suggestion1, suggestion2) {
-  return suggestion2.lastAccessed - suggestion1.lastAccessed;
+async function allRecentlyViewedSuggestions(searchString) {
+  if (SAKA_PLATFORM === 'chrome') {
+    return getChromeRecentlyViewed(searchString);
+  }
+
+  return getFirefoxRecentlyViewed(searchString);
 }
 
 async function filteredRecentlyViewedSuggestions(searchString) {
@@ -39,15 +74,27 @@ async function filteredRecentlyViewedSuggestions(searchString) {
   ].map(tab => ({ ...tab, originalType: tab.type, type: 'recentlyViewed' }));
 }
 
-export default async function recentlyViewedSuggestions(searchString) {
-  if (searchString === '') {
-    const suggestions = await allRecentlyViewedSuggestions(searchString);
-    return suggestions.sort(compareRecentlyViewedSuggestions);
-  }
-
-  return getFilteredSuggestions(searchString, {
+async function getFilteredRecentlyViewedSuggestions(searchString) {
+  const filteredSuggestions = await getFilteredSuggestions(searchString, {
     getSuggestions: filteredRecentlyViewedSuggestions,
     threshold: 0.5,
     keys: ['title', 'url']
   });
+
+  return filteredSuggestions.filter(
+    (suggestion, index) =>
+      filteredSuggestions.findIndex(
+        filteredSuggestion =>
+          filteredSuggestion.url === suggestion.url &&
+          filteredSuggestion.title === suggestion.title
+      ) === index
+  );
+}
+
+export default async function recentlyViewedSuggestions(searchString) {
+  if (searchString === '') {
+    return allRecentlyViewedSuggestions(searchString);
+  }
+
+  return getFilteredRecentlyViewedSuggestions(searchString);
 }
