8e6adfb1434fd589badbeda7ed886ac19bb52aef
Uzair Shamim <uzashamim@gmail.com>
2018-08-14 23:50:09 -0400

    Added jest and dom-testing-library support!

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+    'suggestion_utils/(.*)$': '<rootDir>/src/suggestion_utils/$1',
+    '^lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^.*\\.(css|less|sass|scss)$': '<rootDir>/test/__mocks__/styleMock.scss',
+    '^react-dom/server$':
+      '<rootDir>/node_modules/preact-render-to-string/dist/index.js',
+    '^react-addons-test-utils$':
+      '<rootDir>/node_modules/preact-test-utils/lib/index.js',
+    '^react$': '<rootDir>/node_modules/preact-compat-enzyme/lib/index.js',
+    '^react-dom$': '<rootDir>/node_modules/preact-compat-enzyme/lib/index.js'
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  transform: {
+    '^.+\\.jsx?$': '<rootDir>/jest.transform.js'
+  }
+};
