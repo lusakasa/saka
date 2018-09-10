dc7c6b3dabaedb3eac6ba6e5d71c9ab96218f119
Uzair Shamim <uzashamim@gmail.com>
2018-09-10 15:36:03 -0400

    Cleanup of comments/console log

--- src/suggestion_engine/server/providers/closedTab.js
+++ src/suggestion_engine/server/providers/closedTab.js
@@ -11,44 +11,12 @@ export async function getAllSuggestions() {
   // See: https://github.com/airbnb/javascript/issues/1271
   // Not disabling the rule as this might be fixable in the future using filter.
   // This for loop is needed at the moment as a workaround since filter does not support async.
-  // {
-  //   "lastModified": 1536557259,
-  //   "window": {
-  //     "alwaysOnTop": false,
-  //     "focused": false,
-  //     "incognito": false,
-  //     "sessionId": "345",
-  //     "state": "normal",
-  //     "tabs": [
-  //       {
-  //         "active": false,
-  //         "autoDiscardable": false,
-  //         "discarded": false,
-  //         "favIconUrl": "https://www.google.ca/images/branding/product/ico/googleg_lodp.ico",
-  //         "highlighted": false,
-  //         "incognito": false,
-  //         "index": 0,
-  //         "pinned": false,
-  //         "selected": false,
-  //         "sessionId": "346",
-  //         "title": "test - Google Search",
-  //         "url": "https://www.google.ca/search?q=test&oq=test&aqs=chrome..69i57j0l5.649j0j7&sourceid=chrome&ie=UTF-8",
-  //         "windowId": 0
-  //       }
-  //     ],
-  //     "type": "normal"
-  //   }
-  // }
-  console.log('Session: ', sessions);
-
   for (const session of sessions) {
     if (session.tab && !(await isSakaUrl(session.tab.url))) {
       filteredSessions.push(session);
     } else if (session.window && session.window.tabs) {
-      console.log('TABS: ', session.window);
       for (const tabSession of session.window.tabs) {
         if (tabSession && !(await isSakaUrl(tabSession.url))) {
-          console.log('WIN: ', tabSession);
           filteredSessions.push({
             lastModified: session.window.lastModified,
             tab: tabSession
